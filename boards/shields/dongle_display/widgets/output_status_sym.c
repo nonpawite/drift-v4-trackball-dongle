82265001ee634e8aceb58a694e0476ba938a41e5
Timothy Ryan <84595044+Timception@users.noreply.github.com>
2026-05-09 17:19:41 +0700

    Update output_status_sym.c

--- boards/shields/dongle_display/widgets/output_status_sym.c
+++ boards/shields/dongle_display/widgets/output_status_sym.c
@@ -23,9 +23,9 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_1 uin
 };
 
 const lv_img_dsc_t sym_1 = {
-  .header.cf = LV_IMG_CF_INDEXED_1BIT,
-  .header.always_zero = 0,
-  .header.reserved = 0,
+  .header.cf = LV_COLOR_FORMAT_I1,
+  
+  
   .header.w = 5,
   .header.h = 6,
   .data_size = 14,
@@ -44,9 +44,9 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_2 uin
 };
 
 const lv_img_dsc_t sym_2 = {
-  .header.cf = LV_IMG_CF_INDEXED_1BIT,
-  .header.always_zero = 0,
-  .header.reserved = 0,
+  .header.cf = LV_COLOR_FORMAT_I1,
+  
+  
   .header.w = 5,
   .header.h = 6,
   .data_size = 14,
@@ -65,9 +65,9 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_3 uin
 };
 
 const lv_img_dsc_t sym_3 = {
-  .header.cf = LV_IMG_CF_INDEXED_1BIT,
-  .header.always_zero = 0,
-  .header.reserved = 0,
+  .header.cf = LV_COLOR_FORMAT_I1,
+  
+  
   .header.w = 5,
   .header.h = 6,
   .data_size = 14,
@@ -86,9 +86,9 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_4 uin
 };
 
 const lv_img_dsc_t sym_4 = {
-  .header.cf = LV_IMG_CF_INDEXED_1BIT,
-  .header.always_zero = 0,
-  .header.reserved = 0,
+  .header.cf = LV_COLOR_FORMAT_I1,
+  
+  
   .header.w = 5,
   .header.h = 6,
   .data_size = 14,
@@ -107,9 +107,9 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_5 uin
 };
 
 const lv_img_dsc_t sym_5 = {
-  .header.cf = LV_IMG_CF_INDEXED_1BIT,
-  .header.always_zero = 0,
-  .header.reserved = 0,
+  .header.cf = LV_COLOR_FORMAT_I1,
+  
+  
   .header.w = 5,
   .header.h = 6,
   .data_size = 14,
@@ -128,9 +128,9 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_OK ui
 };
 
 const lv_img_dsc_t sym_ok = {
-  .header.cf = LV_IMG_CF_INDEXED_1BIT,
-  .header.always_zero = 0,
-  .header.reserved = 0,
+  .header.cf = LV_COLOR_FORMAT_I1,
+  
+  
   .header.w = 5,
   .header.h = 5,
   .data_size = 13,
@@ -149,9 +149,9 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_NOK u
 };
 
 const lv_img_dsc_t sym_nok = {
-  .header.cf = LV_IMG_CF_INDEXED_1BIT,
-  .header.always_zero = 0,
-  .header.reserved = 0,
+  .header.cf = LV_COLOR_FORMAT_I1,
+  
+  
   .header.w = 5,
   .header.h = 5,
   .data_size = 13,
@@ -170,9 +170,9 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_OPEN 
 };
 
 const lv_img_dsc_t sym_open = {
-  .header.cf = LV_IMG_CF_INDEXED_1BIT,
-  .header.always_zero = 0,
-  .header.reserved = 0,
+  .header.cf = LV_COLOR_FORMAT_I1,
+  
+  
   .header.w = 5,
   .header.h = 5,
   .data_size = 13,
@@ -194,9 +194,9 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_BT ui
 };
 
 const lv_img_dsc_t sym_bt = {
-  .header.cf = LV_IMG_CF_INDEXED_1BIT,
-  .header.always_zero = 0,
-  .header.reserved = 0,
+  .header.cf = LV_COLOR_FORMAT_I1,
+  
+  
   .header.w = 9,
   .header.h = 14,
   .data_size = 36,
@@ -218,9 +218,9 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_USB u
 };
 
 const lv_img_dsc_t sym_usb = {
-  .header.cf = LV_IMG_CF_INDEXED_1BIT,
-  .header.always_zero = 0,
-  .header.reserved = 0,
+  .header.cf = LV_COLOR_FORMAT_I1,
+  
+  
   .header.w = 9,
   .header.h = 14,
   .data_size = 36,
